a36b39e7b33bc0c6c4c7351ab67649c548045045
Andrew Ravn <ravn.andrew@gmail.com>
2023-10-29 11:54:19 -0600

    solving the fixed sliding window problem for lc\'s DSA class

--- datastructure/arrays/findmaxaverage.go
+++ datastructure/arrays/findmaxaverage.go
@@ -1,5 +1,26 @@
 package arrays
 
-func FindMaxAverage(nums []int, k int) float64 {
+import (
+	"math"
+)
 
+// Fixed sliding window problem
+// You are given an integer array nums consisting of n elements, and an integer k.
+// Find a contiguous subarray whose length is equal to k that has the maximum average
+// value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+func FindMaxAverage(nums []int, k int) float64 {
+	var currSum int
+	var avg float64
+	// build initial window of size k, find it's average
+	for i := 0; i < k; i++ {
+		currSum += nums[i]
+	}
+	avg = float64(currSum) / float64(k)
+	// start sliding the window, compare to previous average and keep largest of two
+	for i := k; i < len(nums); i++ {
+		currSum += nums[i]
+		currSum -= nums[i-k]
+		avg = math.Max(avg, float64(currSum)/float64(k))
+	}
+	return avg
 }

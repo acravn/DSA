5004b7796e4a383097876431e3b1601f94855b3d
Andrew Ravn <ravn.andrew@gmail.com>
2023-10-31 20:48:56 -0600

    finishing up last array problem getAverages from lc dsa course

--- datastructure/arrays/prefixsums.go
+++ datastructure/arrays/prefixsums.go
@@ -32,3 +32,38 @@ func minStartValue(nums []int) int {
 
 	return minVal + 1
 }
+
+// You are given a 0-indexed array nums of n integers, and an integer k.
+// The k-radius average for a subarray of nums centered at some index i with the radius k
+// is the average of all elements in nums between the indices i - k and i + k (inclusive).
+// If there are less than k elements before or after the index i, then the k-radius average is -1.
+// Build and return an array avgs of length n where avgs[i] is the k-radius average for the subarray centered at index i.
+// The average of x elements is the sum of the x elements divided by x, using integer division.
+// The integer division truncates toward zero, which means losing its fractional part.
+func getAverages(nums []int, k int) []int {
+	numsLen := len(nums)
+	avgs := make([]int, numsLen)
+	prefixSum := make([]int, numsLen)
+	windowSize := k*2 + 1
+
+	prefixSum[0] = nums[0]
+	for i := 1; i < numsLen; i++ {
+		prefixSum[i] = nums[i] + prefixSum[i-1]
+	}
+
+	for i, _ := range prefixSum {
+		left, right := i-k, i+k
+		if left < 0 || right > numsLen-1 {
+			avgs[i] = -1
+		} else if k == 0 {
+			avgs[i] = nums[i]
+		} else if left > 0 {
+			avgs[i] = (prefixSum[right] - prefixSum[left-1]) / windowSize
+		} else {
+			avgs[i] = prefixSum[right] / windowSize
+		}
+
+	}
+
+	return avgs
+}
--- datastructure/arrays/prefixsums_test.go
+++ datastructure/arrays/prefixsums_test.go
@@ -58,3 +58,44 @@ func TestMinStartValue(t *testing.T) {
 		}
 	}
 }
+
+func TestGetAverages(t *testing.T) {
+	table := []struct {
+		arr      []int
+		k        int
+		expected []int
+	}{
+		{
+			arr:      []int{7, 4, 3, 9, 1, 8, 5, 2, 6}, // {7, 11, 14, 23, 24, 32, 37, 39, 45}
+			k:        3,
+			expected: []int{-1, -1, -1, 5, 4, 4, -1, -1, -1},
+		},
+		{
+			arr:      []int{100000},
+			k:        0,
+			expected: []int{100000},
+		},
+		{
+			arr:      []int{8},
+			k:        10000,
+			expected: []int{-1},
+		},
+		{
+			arr:      []int{18334, 25764, 19780, 92480, 69842, 73255, 89893},
+			k:        0,
+			expected: []int{18334, 25764, 19780, 92480, 69842, 73255, 89893},
+		},
+		{
+			arr:      []int{40527, 53696, 10730, 66491, 62141, 83909, 78635, 18560},
+			k:        2,
+			expected: []int{-1, -1, 46717, 55393, 60381, 61947, -1, -1},
+		},
+	}
+
+	for _, v := range table {
+		res := getAverages(v.arr, v.k)
+		if !reflect.DeepEqual(res, v.expected) {
+			t.Errorf("Got %v, expected %v", res, v.expected)
+		}
+	}
+}

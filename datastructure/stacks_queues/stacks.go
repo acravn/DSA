43b001ac83bc3aebd7fe53ea3bc1977eedae3819
Andrew Ravn <ravn.andrew@gmail.com>
2024-01-10 06:35:59 -0700

    completing monotonic stack problem on LC

--- datastructure/stacks_queues/stacks.go
+++ datastructure/stacks_queues/stacks.go
@@ -20,3 +20,23 @@ func simplifyPath(path string) string {
 
 	return "/" + strings.Join(stack, "/")
 }
+
+func nextGreaterElement(nums1 []int, nums2 []int) []int {
+	hashMap := make(map[int]int, len(nums1))
+	var stack []int
+	for _, v := range nums2 {
+		for len(stack) > 0 && stack[len(stack)-1] < v {
+			hashMap[stack[len(stack)-1]] = v
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, v)
+	}
+	for i, v := range nums1 {
+		if mapv, ok := hashMap[v]; ok {
+			nums1[i] = mapv
+			continue
+		}
+		nums1[i] = -1
+	}
+	return nums1
+}
--- datastructure/stacks_queues/stacks_test.go
+++ datastructure/stacks_queues/stacks_test.go
@@ -1,6 +1,9 @@
 package stacks_queues
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSimplifyPath(t *testing.T) {
 	tests := []struct {
@@ -29,6 +32,32 @@ func TestSimplifyPath(t *testing.T) {
 		res := simplifyPath(v.input)
 		if res != v.output {
 			t.Errorf("Expected %s, got %s", v.output, res)
+		}
+	}
+}
+
+func TestNextGreaterElement(t *testing.T) {
+	tests := []struct {
+		inputA []int
+		inputB []int
+		output []int
+	}{
+		{
+			inputA: []int{4, 1, 2},
+			inputB: []int{1, 3, 4, 2},
+			output: []int{-1, 3, -1},
+		},
+		{
+			inputA: []int{2, 4},
+			inputB: []int{1, 2, 3, 4},
+			output: []int{3, -1},
+		},
+	}
+
+	for _, v := range tests {
+		res := nextGreaterElement(v.inputA, v.inputB)
+		if !reflect.DeepEqual(res, v.output) {
+			t.Errorf("Expected %v, got %v", v.output, res)
 		}
 	}
 }
